294b8992d29b20d10ef16e7f9fdbafc061fef94f
prdv <prdv001@gmail.com>
2025-04-24 18:01:12 +0530

    my first commit

---
+++ user-service/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the User Service!');
+});
+
+app.get('/users', (req, res) => {
+  res.json([
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' }
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`User Service is running on port ${PORT}`);
+});
